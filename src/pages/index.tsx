ec488696e5ca3bb3a30d8e4e4183df5358a3a76f
EloiGry <eloi.grychta@gmail.com>
2023-02-22 23:00:46 +0100

    header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { signOut, useSession, signIn } from 'next-auth/react';
 import prisma from '@/lib/prismadb';
 import type { GetServerSideProps } from "next";
+import Header from '@/layout/Header';
 
 export const getServerSideProps: GetServerSideProps = async () => {
   const feed = await prisma.product.findMany();
@@ -14,24 +15,11 @@ const IndexPage = ({feed}) => {
   const { data: session} = useSession();
   console.log("session" ,session);
   console.log("feed" ,feed);
-
-
-  if (session) {
     return (
-      <div>
-        Hello, {session.user.name}, <img src={session.user.image} width="30px" height="30px" style={{borderRadius: "50%"}}/>  <br />
-        <button onClick={() => signOut()}>Déconnexion</button>
-      </div>
-    );
-  } else {
-    return (
-      <div>
-        You are not logged in! <br />
-        <button onClick={() => signIn("auth0")}>Se connecter</button>
-        <img src={feed[0].image}/>
-      </div>
-    );
-  }
-};
+      <>
+        <Header/>
+      </>
+    )
+  };
 
 export default IndexPage;
