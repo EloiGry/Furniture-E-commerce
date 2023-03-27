9aae572feba66540e13d9145ed9dd310453b2907
EloiGry <eloi.grychta@gmail.com>
2023-03-27 19:23:00 +0200

    modif

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  babel: {
-    presets: ["next/babel", "@babel/preset-react"],
-    plugins: [
-      ["@babel/plugin-transform-react-jsx", { "runtime": "automatic" }]
-    ]
-  }
 }
 
 module.exports = nextConfig
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,6 @@
 import { NextApiHandler } from 'next';
 import NextAuth, { NextAuthOptions } from "next-auth" ;
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
-// import GoogleProvider from "next-auth/providers/google"
 import Auth0Provider from "next-auth/providers/auth0";
 import prisma from "../../../lib/prismadb"
 
@@ -12,10 +11,6 @@ export default authHandler;
 
 const options: NextAuthOptions = {
   providers: [
-    // GoogleProvider({
-    //   clientId: process.env.GOOGLE_CLIENT_ID,
-    //   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    // }),
     Auth0Provider({
       clientId: process.env.AUTH0_CLIENT_ID,
       clientSecret: process.env.AUTH0_CLIENT_SECRET,
